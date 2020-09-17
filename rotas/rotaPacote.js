7b14d43319d6b236be67f9c83376e39b0e30a492
Gustavo Leão <gustavo.leao.nogueira.2017@gmail.com>
2020-09-16 21:02:46 -0300

    rota de pacote criada

---
+++ rotas/rotaPacote.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const controller = require('../controller/controllerPacote');
+
+router.get('/', controller.listar);
+router.get('/search', controller.procurar);
+router.get('/:id', controller.buscarPorId);
+router.put('/:id', controller.atualizar);
+router.delete('/:id', controller.deletar); 
+router.post('/', controller.inserir);
+
+module.exports = router;
