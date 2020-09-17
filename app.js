6ca1c080adac65e82d9c2c9dcb2587b17fddfe56
Gustavo Leão <gustavo.leao.nogueira.2017@gmail.com>
2020-09-16 21:01:41 -0300

    middlware criado

--- app.js
+++ app.js
@@ -4,6 +4,19 @@ const port = 3000;
 
 const rotaPacote = require('./rotas/rotaPacote');
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+app.use((req, res, next) => {
+    console.warn("####################################");
+    console.warn(`# Hora da requisicao:${Date.now()} `);
+    console.warn(`# Método: ${req.method}            `);
+    console.warn("####################################");
+});
+
+app.use('/api/pacotes');
+
 app.listen(port, () => {
     console.log(`Entrar: http://localhost:${port}`);
 });
