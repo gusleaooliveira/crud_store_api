d056428c9f6e6fc5e8ae07d1a52c0b6f6dcdb0d9
Gustavo Leão <gustavo.leao.nogueira.2017@gmail.com>
2020-09-16 23:16:54 -0300

    mongoose adicionado

--- app.js
+++ app.js
@@ -1,21 +1,38 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const mongoose = require('mongoose');
 
 const rotaPacote = require('./rotas/rotaPacote');
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+mongoose.connect('mongodb://localhost/api_pacote', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+}).then(() => {
+    console.log("#################");
+    console.log("# BD conectado!");
+    console.log("#################");
+}).catch((error) => {
+    console.error("#################");
+    console.error("# Erro ao conectar no BD!");
+    console.error("#################");
+});
+
+mongoose.Promise = global.Promise;
 
 app.use((req, res, next) => {
     console.warn("####################################");
     console.warn(`# Hora da requisicao:${Date.now()} `);
     console.warn(`# Método: ${req.method}            `);
     console.warn("####################################");
+    next();
 });
 
-app.use('/api/pacotes');
+app.use('/api/pacotes', rotaPacote);
 
 app.listen(port, () => {
     console.log(`Entrar: http://localhost:${port}`);
