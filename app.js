b67aa1f27aa589e9660b7cf973f4ff0489e55c8f
Gustavo Leão <gustavo.leao.nogueira.2017@gmail.com>
2020-09-23 20:12:06 -0300

    proposta do trabalho iniciada

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
+require("dotenv-safe").config();
 const express = require('express');
 const app = express();
 const port = 3000;
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const jwt = require("jsonwebtoken");
 
 const rotaPacote = require('./rotas/rotaPacote');
 const rotaUsuario = require('./rotas/rotaUsuario');
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
 
 mongoose.connect('mongodb://localhost/api_pacote', {
     useNewUrlParser: true,
@@ -25,10 +29,30 @@ mongoose.connect('mongodb://localhost/api_pacote', {
 
 mongoose.Promise = global.Promise;
 
+function verifiqueJwt(req, res, next) {
+    let token = req.headers["x-access-token"];
+    if(!token){
+        console.error("# Nenhum token fornecido! ");
+        return res.status(401).json({auth: false, msg: "Nenum token fornecido!"});
+    }
+
+    jwt.verify(token, process.env.SECRET, (err, decoded) =>{
+        if(err){
+            console.error("# Falha ao autenticar! ");
+            return res.json(500).json({auth: false, msg: "Falha ao autenticar! "});
+        }
+        req.userId = decoded.id;
+
+        next();
+    });
+};
+
+
 app.use((req, res, next) => {
     console.warn("####################################");
     console.warn(`# Hora da requisicao:${Date.now()} `);
     console.warn(`# Método: ${req.method}            `);
+    verifiqueJwt();
     console.warn("####################################");
     next();
 });
--- rotas/rotaUsuario.js
+++ rotas/rotaUsuario.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const controller = require('../controller/controllerPacote');
+const controller = require('../controller/controllerUsuario');
 
-
-router.get('/', );
-router.get('/search', );
-router.get('/:id', );
-router.put('/:id', );
-router.delete('/:id', );
-router.post('/', );
+router.get('/', controller.listar);
+router.get('/search', controller.procurar);
+router.get('/:id', controller.buscarPorId);
+router.put('/:id', controller.atualizar);
+router.delete('/:id', controller.deletar); 
+router.post('/', controller.inserir);
 
 module.exports = router;
